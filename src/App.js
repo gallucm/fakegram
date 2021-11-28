c8dbd3a5e317ac30a49abcd468a87e3ee8fccfa0
Cristian Galluccio <gallucciocristian@gmail.com>
2021-11-28 16:23:31 -0300

    advanced Navbar and others

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 
 import { Navbar } from './components/ui/Navbar';
 import { Account } from './pages/Account';
+import { Login } from './pages/Login';
 import { Index } from './pages/Main';
 import { NotFound } from './pages/NotFound';
 import { Profile } from './pages/Profile';
@@ -12,8 +13,9 @@ export const App = () => {
     <Navbar>
       <Switch>
         <Route exact path="/" component={Index} />
-        <Route exact path="/:id" component={Profile} />
-        <Route exact path="/:id/details" component={Account} />
+        <Route exact path="/p/:id" component={Profile} />
+        <Route exact path="/account" component={Account} />
+        <Route exact path="/login" component={Login} />
         <Route path="*" component={NotFound} />
       </Switch>
     </Navbar>
--- src/components/ui/MainNavigation.jsx
+++ src/components/ui/MainNavigation.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import classes from './MainNavigation.module.css';
 import avatar from '../../assets/images/avatar.png';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export const MainNavigation = () => {
+    const history = useHistory();
+
+    const inputSearchRef = useRef();
+
+    const handleSearchClick = () => {
+        // const input = inputSearchRef.current.value;
+    }
+
+    const handleHomeClick = () => {
+        history.push('/');
+    }
+
+    const handleLogoutClick = () => {
+        history.replace('/login');
+    }
+
     return (
         <header className={classes.header}>
-            <h1 className={classes.logo}>Fakegram</h1>
-            <input type="text" className={classes.input_search}/>
-            <img src={avatar} alt="Avatar" className={classes.avatar}/>
+            <h1 className={classes.logo} onClick={handleHomeClick}>Fakegram</h1>
+            <input type="text" className={classes.input_search} ref={inputSearchRef} onKeyUp={handleSearchClick} />
+            <div className={classes.dropdown}>
+                <img src={avatar} alt="Avatar" className={classes.avatar} />
+                <div className={classes.drowpdown__content}>
+                    <div className={classes.link}>
+                        <Link to="/p/1234">Perfil</Link>
+                    </div>
+                    <div className={classes.link}>
+                        <Link to="/account">Ajustes</Link>
+                    </div>
+                    <div className={classes.link} onClick={handleLogoutClick}>
+                        Salir
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
