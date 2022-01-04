90fa882d02604ad9fa2a13a1387cae4aafa4816d
Cristian Galluccio <gallucciocristian@gmail.com>
2022-01-04 16:40:59 -0300

    little fix on profile button (navbar)

--- src/components/ui/NavbarIcons.jsx
+++ src/components/ui/NavbarIcons.jsx
@@ -7,32 +7,6 @@ import MapsUgcOutlinedIcon from '@mui/icons-material/MapsUgcOutlined';
 import classes from './NavbarIcons.module.css';
 
 export const NavbarIcons = () => {
-
-    // const dispatch = useDispatch();
-
-    // const [imageLoad, setImageLoad] = useState(false);
-
-    // const showOptionsMenuHandler = () => {
-    //     const optionsDiv = document.querySelector(`.${classes.options}`);
-    //     optionsDiv.classList.toggle(classes.show);
-    // }
-
-    // const handleImageLoad = () => {
-    //     setImageLoad(true);
-    // }
-
-    // const handleLogoutClick = () => {
-    //     dispatch(authActions.logout());
-    //     dispatch(profileActions.removeUser());
-    // }
-
-
-
-    // const hideOptionsMenuHandler = () => {
-    //     const optionsDiv = document.querySelector(`.${classes.options}`);
-    //     optionsDiv.classList.toggle(classes.hide);
-    // }
-
     return (
             <div className={classes.icons}>
                 <Link to="/" className={classes.link_default}>
@@ -42,19 +16,6 @@ export const NavbarIcons = () => {
                     <MapsUgcOutlinedIcon className={classes.icon} titleAccess="Mensajes" />
                 </Link>
                 <FavoriteBorderIcon className={classes.icon} titleAccess="Notificaciones" />
-                
-            {/* <div className={classes.options}>
-                    <div className={classes.link}>
-                        <Link to="/gallucm" onClick={hideOptionsMenuHandler}>Perfil</Link>
-                    </div>
-                    <div className={classes.link}>
-                        <Link to="/account" onClick={hideOptionsMenuHandler}>Configuración</Link>
-                    </div>
-                    <div className={classes.link} onClick={handleLogoutClick}>
-                        Salir
-                    </div>
-                </div>
-            </div> */}
             </div>
     )
 }
