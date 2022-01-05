4791825ae78f3089f35a03d216d23ab7ae970b0a
Cristian Galluccio <gallucciocristian@gmail.com>
2022-01-04 21:16:17 -0300

    Icons black on over added

--- src/components/ui/NavbarButtonAddPhoto.jsx
+++ src/components/ui/NavbarButtonAddPhoto.jsx
@@ -1,15 +1,22 @@
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 
 import classes from './NavbarIcons.module.css';
+import { useState } from 'react';
 
 export const NavbarButtonAddPhoto = () => {
+    const [addPhotoDark, setAddPhotoDark] = useState(false);
 
     const handleOpenModal = () => {
         console.log('open modal')
     }
     return (
         <div>
-            <AddPhotoAlternateOutlinedIcon className={classes.icon} titleAccess="Subir foto" onClick={handleOpenModal} />
+           {
+               addPhotoDark 
+               ? <AddPhotoAlternateIcon className={classes.icon} titleAccess="Subir foto" onClick={handleOpenModal} onMouseLeave={() => {setAddPhotoDark(false)}}/>
+               : <AddPhotoAlternateOutlinedIcon className={classes.icon} titleAccess="Subir foto" onClick={handleOpenModal} onMouseOver={() => {setAddPhotoDark(true)}}/>
+           } 
         </div>
     )
 }
--- src/components/ui/NavbarIcons.jsx
+++ src/components/ui/NavbarIcons.jsx
@@ -1,24 +1,46 @@
 import { Link } from 'react-router-dom';
 
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import MapsUgcOutlinedIcon from '@mui/icons-material/MapsUgcOutlined';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import HomeIcon from '@mui/icons-material/Home';
+
+import MapsUgcOutlinedIcon from '@mui/icons-material/MapsUgcOutlined';
+import MapsUgcIcon from '@mui/icons-material/MapsUgc';
 
 import classes from './NavbarIcons.module.css';
 import { NavbarButtonAddPhoto } from './NavbarButtonAddPhoto';
+import { useState } from 'react';
 
 export const NavbarIcons = () => {
+
+    const [homeDark, setHomeDark] = useState(false);
+    const [msgDark, setMsgDark] = useState(false);
+    const [favoriteDark, setFavoriteDark] = useState(false);
+
     return (
-            <div className={classes.icons}>
-                <Link to="/" className={classes.link_default}>
-                    <HomeOutlinedIcon className={classes.icon} titleAccess="Inicio" />
-                </Link>
-                <Link to="/messages" className={classes.link_default}>
-                    <MapsUgcOutlinedIcon className={classes.icon} titleAccess="Mensajes" />
-                </Link>
-                <NavbarButtonAddPhoto/>
-                <FavoriteBorderIcon className={classes.icon} titleAccess="Notificaciones" />
-            </div>
+        <div className={classes.icons}>
+            <Link to="/" className={classes.link_default}>
+                {
+                    homeDark
+                        ? <HomeIcon className={classes.icon} titleAccess="Inicio" onMouseLeave={() => setHomeDark(false)} />
+                        : <HomeOutlinedIcon className={classes.icon} titleAccess="Inicio" onMouseOver={() => setHomeDark(true)} />
+                }
+            </Link>
+            <Link to="/messages" className={classes.link_default}>
+                {
+                    msgDark
+                        ? <MapsUgcIcon className={classes.icon} titleAccess="Mensajes" onMouseLeave={() => setMsgDark(false)} />
+                        : <MapsUgcOutlinedIcon className={classes.icon} titleAccess="Mensajes" onMouseOver={() => setMsgDark(true)} />
+                }
+            </Link>
+            <NavbarButtonAddPhoto />
+            {
+                favoriteDark
+                    ? <FavoriteIcon className={classes.icon} titleAccess="Notificaciones" onMouseLeave={() => setFavoriteDark(false)} />
+                    : <FavoriteBorderIcon className={classes.icon} titleAccess="Notificaciones" onMouseOver={() => setFavoriteDark(true)} />
+            }
+        </div>
     )
 }
